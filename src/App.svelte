32a4b5551a495554f27a795ddb8ca934a1141d0b
chucknerd <chucknerd1@gmail.com>
2020-02-13 07:12:53 -0600

    buttons and started logic

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
   const title = "Tic Tac Toe";
+  import Board from "./Board.svelte";
 </script>
 
 <svelte:head>
@@ -7,3 +8,4 @@
 </svelte:head>
 
 <h1>{title}</h1>
+<Board />
--- src/Board.svelte
+++ src/Board.svelte
@@ -1,6 +1,16 @@
 <script>
   import Square from "./Square.svelte";
   let squares = [null, null, null, null, null, null, null, null, null];
+
+  let xIsNext = true;
+  $: status = "Next PLayer: " + (xIsNext ? "X" : "O");
+
+  function handleClick(i) {
+    if (!squares[i]) {
+      squares[i] = isNext ? "X" : "O";
+      xIsNext = !xIsNext;
+    }
+  }
 </script>
 
 <style>
